143d5369e962ae1b8316e051893a361ebf7683d1
Dmitry Kulinchenko <dima.kul22@mail.ru>
2020-09-15 23:35:29 +0300

    master

---
+++ src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        final double pi = 3.14;
+        double cat1, cat2, c, r, s;
+        Scanner num = new Scanner(System.in);
+        System.out.println("Ввдеите длину катета ");
+        cat1 = num.nextDouble();
+
+
+        cat2 = cat1;
+        System.out.println("Длина другого катета = " + cat2);
+
+        c = Math.sqrt(cat1 * cat1 + cat2 * cat2);
+        System.out.println("Длина гипотенузы = " + c);
+
+        r = c / 2;
+        System.out.println("Радиус окружности равен = " + r);
+
+        s = pi * r * r;
+        System.out.printf("Для круга R = %1$.2f площадь s = %2$.2f",r,s );
+
+    }
+}
+
